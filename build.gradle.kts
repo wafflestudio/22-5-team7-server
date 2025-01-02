9879429dd47a140367303dac0dae8515605a70ad
Junghoon Kim <jafarock@naver.com>
2025-01-02 15:26:23 +0900

    :bug: fixed unmatching name in local and github secrets

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
---
+++ src/main/kotlin/com/toyProject7/karrot/configuration/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.toyProject7.karrot.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { registry ->
+                registry
+                    .requestMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .oauth2Login { customizer -> customizer }
+            .formLogin { customizer -> customizer }
+
+        return http.build()
+    }
+}
