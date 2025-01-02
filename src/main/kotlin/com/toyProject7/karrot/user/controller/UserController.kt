7c07bbc9bea170134f0b8001c87d947b74c301d9
Junghoon Kim <jafarock@naver.com>
2025-01-02 20:21:05 +0900

    :hammer: removed sensible data

--- src/main/kotlin/com/toyProject7/karrot/user/controller/UserController.kt
+++ src/main/kotlin/com/toyProject7/karrot/user/controller/UserController.kt
@@ -1,8 +1,9 @@
 package com.toyProject7.karrot.user.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.security.Principal
 
 @RestController
 class UserController {
@@ -12,7 +13,19 @@ class UserController {
     }
 
     @RequestMapping("/user")
-    fun user(principal: Principal): Principal {
-        return principal
+    fun user(
+        @AuthenticationPrincipal oAuth2User: OAuth2User,
+    ): Map<String, Any?> {
+        // Extracting attributes
+        val attributes = oAuth2User.attributes
+
+        // Extract username and email
+        val username = attributes["name"] as String? // You can customize the key to get the desired username
+        val email = attributes["email"] as String?
+
+        return mapOf(
+            "username" to username,
+            "email" to email,
+        )
     }
 }
