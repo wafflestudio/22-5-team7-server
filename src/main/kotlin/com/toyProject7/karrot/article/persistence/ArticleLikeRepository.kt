9387c2495194ed8485173ebf43e34e552718da0a
wonseok <wonseok1122@gmail.com>
2025-01-07 17:37:11 +0900

    🔥 Remove ArticleLikeEntity/ArticleLikeRepository

--- src/main/kotlin/com/toyProject7/karrot/article/persistence/ArticleLikeRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.toyProject7.karrot.article.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ArticleLikeRepository : JpaRepository<ArticleLikeEntity, String> {
-    fun findAllByUserId(userId: String): List<ArticleLikeEntity>
-}
