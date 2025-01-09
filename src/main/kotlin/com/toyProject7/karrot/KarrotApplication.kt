2f5a47c0950fa6eec5acbc9c4a570eb385d1bc0f
Junghoon Kim <jafarock@naver.com>
2025-01-09 21:10:12 +0900

    :bug: removed entity check annotation so it does not only check user entities

--- src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
+++ src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
@@ -1,13 +1,11 @@
 package com.toyProject7.karrot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 @EnableWebSecurity
-@EntityScan(basePackages = ["com.toyProject7.karrot.user.persistence"])
 class KarrotApplication
 
 fun main(args: Array<String>) {
