e28c4edc897b8af7b1ca8bde01947d2ecce9db2b
Junghoon Kim <jafarock@naver.com>
2025-01-09 22:36:42 +0900

    :bug: fixed unauthenticated redirection to login page and also no more spring auto-created login page

--- src/main/kotlin/com/toyProject7/karrot/SecurityConfig.kt
+++ src/main/kotlin/com/toyProject7/karrot/SecurityConfig.kt
@@ -5,9 +5,11 @@ import com.toyProject7.karrot.socialLogin.handler.CustomAuthenticationSuccessHan
 import com.toyProject7.karrot.socialLogin.service.SocialLoginUserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
@@ -25,9 +27,16 @@ class SecurityConfig(
                 registry
                     .requestMatchers(
                         "/api/auth/**",
-                        "oauth2/**",
+                        "/oauth2/**",
+                        "/auth/**",
                     ).permitAll()
                     .anyRequest().authenticated()
+            }
+            // Disable form login
+            .formLogin { formLogin -> formLogin.disable() }
+            // Configure exception handling
+            .exceptionHandling { exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             }
             .oauth2Login { oauth2login ->
                 oauth2login
