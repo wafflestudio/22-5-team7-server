9c71412ebb72f283482ef7accc48c96c2187cd80
Junghoon Kim <jafarock@naver.com>
2025-01-13 19:18:53 +0900

    :wrench: added tests for different secrets too

--- src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
+++ src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
@@ -10,6 +10,46 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 class KarrotApplication {
     @PostConstruct
     fun verifyEnvVariables() {
+        if (System.getenv("JWT_SECRET_KEY") == null) {
+            println("JWT_SECRET_KEY is not set")
+        } else {
+            println("JWT_SECRET_KEY is set")
+        }
+        if (System.getenv("AWS_ACCESS_KEY_ID") == null) {
+            println("AWS_ACCESS_KEY_ID is not set")
+        } else {
+            println("AWS_SECRET_ACCESS_KEY is set")
+        }
+        if (System.getenv("AWS_SECRET_ACCESS_KEY") == null) {
+            println("AWS_SECRET_ACCESS_KEY is not set")
+        } else {
+            println("AWS_SECRET_ACCESS_KEY is set")
+        }
+        if (System.getenv("AWS_S3_BUCKET") == null) {
+            println("AWS_S3_BUCKET is not set")
+        } else {
+            println("AWS_S3_BUCKET is set")
+        }
+        if (System.getenv("AWS_REGION") == null) {
+            println("AWS_REGION is not set")
+        } else {
+            println("AWS_REGION is set")
+        }
+        if (System.getenv("GOOGLE_CLI_SECRET") == null) {
+            println("GOOGLE_CLI_SECRET is not set")
+        } else {
+            println("GOOGLE_CLI_SECRET is set")
+        }
+        if (System.getenv("NAVER_CLI_SECRET") == null) {
+            println("NAVER_CLI_SECRET is not set")
+        } else {
+            println("NAVER_CLI_SECRET is set")
+        }
+        if (System.getenv("KAKAO_CLI_SECRET") == null) {
+            println("KAKAO_CLI_SECRET is not set")
+        } else {
+            println("KAKAO_CLI_SECRET is set")
+        }
         if (System.getenv("GOOGLE_CLIENT_ID") == null) {
             println("GOOGLE_CLIENT_ID is not set")
         } else {
