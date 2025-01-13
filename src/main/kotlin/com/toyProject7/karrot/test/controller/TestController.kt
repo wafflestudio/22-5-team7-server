76ae2b4a140bd1d46a3c88843f31f8f6a6dde61d
Junghoon Kim <jafarock@naver.com>
2025-01-13 21:47:53 +0900

    :pencil: added test to oauth2 google in testController

--- src/main/kotlin/com/toyProject7/karrot/test/controller/TestController.kt
+++ src/main/kotlin/com/toyProject7/karrot/test/controller/TestController.kt
@@ -25,6 +25,12 @@ class TestController {
         val response = TestResponse(request.userId, request.id, request.title, request.body)
         return ResponseEntity.ok(response)
     }
+
+    @GetMapping("/test-oauth2-authorization-google")
+    fun testOAuth2Authorization(): ResponseEntity<String> {
+        println("Accessed /test-oauth2-authorization-google")
+        return ResponseEntity.ok("Test OAuth2 Authorization Accessed")
+    }
 }
 
 data class TestResponse(
