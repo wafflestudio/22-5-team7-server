96ebf5184f2e83ce59ae3fb58b9f212e956bbdb6
Junghoon Kim <jafarock@naver.com>
2025-01-13 19:12:25 +0900

    :wrench: added to check if github secrets got passed

--- src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
+++ src/main/kotlin/com/toyProject7/karrot/KarrotApplication.kt
@@ -3,10 +3,31 @@ package com.toyProject7.karrot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import jakarta.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableWebSecurity
-class KarrotApplication
+class KarrotApplication{
+    @PostConstruct
+    fun verifyEnvVariables() {
+        if (System.getenv("GOOGLE_CLIENT_ID") == null) {
+            println("GOOGLE_CLIENT_ID is not set")
+        } else {
+            println("GOOGLE_CLIENT_ID is set")
+        }
+        // Repeat for other environment variables
+        if (System.getenv("NAVER_CLIENT_ID") == null) {
+            println("NAVER_CLIENT_ID is not set")
+        } else {
+            println("NAVER_CLIENT_ID is set")
+        }
+        if (System.getenv("KAKAO_CLIENT_ID") == null) {
+            println("KAKAO_CLIENT_ID is not set")
+        } else {
+            println("KAKAO_CLIENT_ID is set")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KarrotApplication>(*args)
