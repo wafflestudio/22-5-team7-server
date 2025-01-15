db8b239a16114e11975edea045ededeca1a589ff
wonseok <wonseok1122@gmail.com>
2025-01-15 22:57:24 +0900

    🐛 Fix profileEntity joinColumn to joinTable

--- src/main/kotlin/com/toyProject7/karrot/profile/persistence/ProfileEntity.kt
+++ src/main/kotlin/com/toyProject7/karrot/profile/persistence/ProfileEntity.kt
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
 import jakarta.persistence.ManyToMany
 import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
@@ -24,6 +25,10 @@ class ProfileEntity(
     @OneToMany(mappedBy = "profile", cascade = [CascadeType.ALL])
     var manners: MutableList<MannerEntity> = mutableListOf(),
     @ManyToMany
-    @JoinColumn(name = "reviews")
+    @JoinTable(
+        name = "profile_review",
+        joinColumns = [JoinColumn(name = "profile_id")],
+        inverseJoinColumns = [JoinColumn(name = "review_id")]
+    )
     var reviews: MutableList<ReviewEntity> = mutableListOf(),
 )
