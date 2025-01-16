93ea0b87874bbf23e3ea7e55583245c55c3017c4
Junyong Lee <leejunyong02@snu.ac.kr>
2025-01-16 18:07:49 +0900

    :sparkles: postComment function

--- src/main/kotlin/com/toyProject7/karrot/comment/controller/CommentController.kt
+++ src/main/kotlin/com/toyProject7/karrot/comment/controller/CommentController.kt
@@ -1,6 +1,12 @@
 package com.toyProject7.karrot.comment.controller
 
 import com.toyProject7.karrot.comment.service.CommentService
+import com.toyProject7.karrot.user.AuthUser
+import com.toyProject7.karrot.user.controller.User
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class CommentController(
     private val commentService: CommentService,
+) {
+    @PostMapping("/comment/post/{feedId}")
+    fun postComment(
+        @RequestBody request: CommentRequest,
+        @PathVariable("feedId") feedId: Long,
+        @AuthUser user: User,
+    ): ResponseEntity<Comment> {
+        val comment = commentService.postComment(request, feedId, user.id)
+        return ResponseEntity.ok(comment)
+    }
+}
+
+data class CommentRequest(
+    val content: String,
 )
--- src/main/kotlin/com/toyProject7/karrot/comment/service/CommentService.kt
+++ src/main/kotlin/com/toyProject7/karrot/comment/service/CommentService.kt
@@ -1,11 +1,40 @@
 package com.toyProject7.karrot.comment.service
 
+import com.toyProject7.karrot.comment.controller.Comment
+import com.toyProject7.karrot.comment.controller.CommentRequest
+import com.toyProject7.karrot.comment.persistence.CommentEntity
 import com.toyProject7.karrot.comment.persistence.CommentLikesRepository
 import com.toyProject7.karrot.comment.persistence.CommentRepository
+import com.toyProject7.karrot.feed.service.FeedService
+import com.toyProject7.karrot.user.service.UserService
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
 
 @Service
 class CommentService(
-    val commentRepository: CommentRepository,
-    val commentLikesRepository: CommentLikesRepository,
-)
+    private val commentRepository: CommentRepository,
+    private val commentLikesRepository: CommentLikesRepository,
+    private val userService: UserService,
+    private val feedService: FeedService,
+) {
+    @Transactional
+    fun postComment(
+        request: CommentRequest,
+        feedId: Long,
+        id: String,
+    ): Comment {
+        val feed = feedService.getFeedEntityById(feedId)
+        val user = userService.getUserEntityById(id)
+        val commentEntity =
+            CommentEntity(
+                user = user,
+                feed = feed,
+                content = request.content,
+                createdAt = Instant.now(),
+                updatedAt = Instant.now(),
+            )
+        commentRepository.save(commentEntity)
+        return Comment.fromEntity(commentEntity)
+    }
+}
