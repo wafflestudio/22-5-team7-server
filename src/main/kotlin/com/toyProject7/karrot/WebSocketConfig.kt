24ed0d6fb82ff5a137fb865e78476b6d338e824c
wonseok <wonseok1122@gmail.com>
2025-01-16 17:33:50 +0900

    ✨ Define message, add Controller

--- src/main/kotlin/com/toyProject7/karrot/WebSocketConfig.kt
+++ src/main/kotlin/com/toyProject7/karrot/WebSocketConfig.kt
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS()
     }
--- src/main/kotlin/com/toyProject7/karrot/chatroom/controller/ChatroomController.kt
+++ src/main/kotlin/com/toyProject7/karrot/chatroom/controller/ChatroomController.kt
@@ -1,3 +1,20 @@
 package com.toyProject7.karrot.chatroom.controller
 
-class ChatroomController
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ChatroomController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    fun sendMessage(chatMessage: ChatMessage): ChatMessage {
+        return chatMessage
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    fun addUser(chatMessage: ChatMessage): ChatMessage {
+        return chatMessage.copy(content = "${chatMessage.sender.nickname}님이 입장했습니다.")
+    }
+}
