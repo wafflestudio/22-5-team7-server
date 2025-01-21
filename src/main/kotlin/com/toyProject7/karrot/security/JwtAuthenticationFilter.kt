b0217c7374c99f6a9a9b3a54e42ac7df73bb5e16
Junghoon Kim <jafarock@naver.com>
2025-01-22 01:09:42 +0900

    :art: lint

--- src/main/kotlin/com/toyProject7/karrot/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/toyProject7/karrot/security/JwtAuthenticationFilter.kt
@@ -5,7 +5,6 @@ import com.toyProject7.karrot.user.service.UserService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
