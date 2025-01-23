d98d39f112a7d5bc275f17e104c09a21c5c08e2a
Junghoon Kim <jafarock@naver.com>
2025-01-23 20:01:58 +0900

    :wrench: specified authuser class

--- src/main/kotlin/com/toyProject7/karrot/user/AuthUser.kt
+++ src/main/kotlin/com/toyProject7/karrot/user/AuthUser.kt
@@ -1,5 +1,39 @@
 package com.toyProject7.karrot.user
 
+import com.toyProject7.karrot.user.persistence.UserPrincipal
+import com.toyProject7.karrot.user.service.UserService
+import org.springframework.core.MethodParameter
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class AuthUser
+
+@Component
+class AuthUserResolver(
+    private val userService: UserService,
+) : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(AuthUser::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?,
+    ): Any? {
+        val authentication = SecurityContextHolder.getContext().authentication
+        if (authentication != null && authentication.principal is UserPrincipal) {
+            val userPrincipal = authentication.principal as UserPrincipal
+            return userService.getUserEntityById(userPrincipal.id)
+        }
+        throw AccessDeniedException("User not authenticated")
+    }
+}
