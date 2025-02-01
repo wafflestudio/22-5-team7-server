ea48d4b56306a5ee83898ea5f3d3e8088a0a4f03
wonseok <wonseok1122@gmail.com>
2025-02-01 15:04:45 +0900

    🔨 Block review/manner posting

--- src/main/kotlin/com/toyProject7/karrot/article/ArticleException.kt
+++ src/main/kotlin/com/toyProject7/karrot/article/ArticleException.kt
@@ -22,3 +22,15 @@ class ArticlePermissionDeniedException : ArticleException(
     httpStatusCode = HttpStatus.UNAUTHORIZED,
     msg = "Permission denied",
 )
+
+class ArticleIsNotEndException : ArticleException(
+    errorCode = 0,
+    httpStatusCode = HttpStatus.BAD_REQUEST,
+    msg = "Article isn't end.",
+)
+
+class YouAreNotBuyerOrSellerException : ArticleException(
+    errorCode = 0,
+    httpStatusCode = HttpStatus.BAD_REQUEST,
+    msg = "You are not buyer or seller",
+)
