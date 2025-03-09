3e328f2cd0a06da998a9904ebed75ce12c9a687c
Junghoon Kim <jafarock@naver.com>
2025-03-09 13:22:12 +0900

    added / in public paths

--- src/main/kotlin/com/toyProject7/karrot/security/SecurityConstants.kt
+++ src/main/kotlin/com/toyProject7/karrot/security/SecurityConstants.kt
@@ -3,6 +3,7 @@ package com.toyProject7.karrot.security
 object SecurityConstants {
     val PUBLIC_PATHS =
         arrayOf(
+            "/",
             "/api/auth/**",
             "/oauth2/**",
             "/auth/**",
