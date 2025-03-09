7d2f54b10e5edfa620406394b5bfe357f0fc71b7
Junghoon Kim <jafarock@naver.com>
2025-03-09 13:26:56 +0900

    removed / from public_paths since it has no use

--- src/main/kotlin/com/toyProject7/karrot/security/SecurityConstants.kt
+++ src/main/kotlin/com/toyProject7/karrot/security/SecurityConstants.kt
@@ -3,7 +3,6 @@ package com.toyProject7.karrot.security
 object SecurityConstants {
     val PUBLIC_PATHS =
         arrayOf(
-            "/",
             "/api/auth/**",
             "/oauth2/**",
             "/auth/**",
